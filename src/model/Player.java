7f78c82e86d9c2651aacf1b654caf6691ce0e9f3
Alexamiot <44364111+Alexamiot@users.noreply.github.com>
2019-01-12 15:40:41 +0100

    changement du printboard

--- src/model/Player.java
+++ src/model/Player.java
@@ -65,7 +65,18 @@ public class Player {
 	public void printBoard() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				System.out.print("\t"+ board[j][i].getType());
+				if(board[j][i].getType().equals("Montagne")) {
+					System.out.print("\t"+ "Mont");
+				}
+				else if(board[j][i].getType().equals("Prairie")) {
+					System.out.print("\t"+ "Prairy");
+				}
+				else {
+					System.out.print("\t"+ board[j][i].getType());
+				}
+				if(board[j][i].getCrown()!=0) {
+					System.out.print(board[j][i].getCrown());
+				}
 			}
 			System.out.println();
 		}
